b1b3ae64c0cc50dc4d29dde4866e7405d90f79bd
Eric Sauter <esauter5@gmail.com>
2017-09-01 12:36:18 -0400

    add build folder since we aren't using a package

---
+++ build/components/Picker.js
@@ -0,0 +1,17 @@
+var _react=require('react');var _react2=_interopRequireDefault(_react);
+var _createMockComponent=require('./createMockComponent');var _createMockComponent2=_interopRequireDefault(_createMockComponent);
+var _propTypes=require('prop-types');var _propTypes2=_interopRequireDefault(_propTypes);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{'default':obj};}
+
+var Picker=_react2['default'].createClass({displayName:'Picker',
+propTypes:{
+children:_propTypes2['default'].node},
+
+statics:{
+Item:(0,_createMockComponent2['default'])('Picker.Item')},
+
+render:function(){function render(){
+return null;
+}return render;}()});
+
+
+module.exports=Picker;
---
+++ build/components/Text.js
@@ -0,0 +1,54 @@
+
+
+
+var _react=require('react');var _react2=_interopRequireDefault(_react);
+var _StyleSheetPropType=require('../propTypes/StyleSheetPropType');var _StyleSheetPropType2=_interopRequireDefault(_StyleSheetPropType);
+var _TextStylePropTypes=require('../propTypes/TextStylePropTypes');var _TextStylePropTypes2=_interopRequireDefault(_TextStylePropTypes);
+var _NativeMethodsMixin=require('../mixins/NativeMethodsMixin');var _NativeMethodsMixin2=_interopRequireDefault(_NativeMethodsMixin);
+var _propTypes=require('prop-types');var _propTypes2=_interopRequireDefault(_propTypes);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{'default':obj};}
+
+var stylePropType=(0,_StyleSheetPropType2['default'])(_TextStylePropTypes2['default']);/**
+ * https://github.com/facebook/react-native/blob/master/Libraries/Text/Text.js
+ */var Text=_react2['default'].createClass({displayName:'Text',
+propTypes:{
+/**
+     * Used to truncate the text with an ellipsis after computing the text
+     * layout, including line wrapping, such that the total number of lines
+     * does not exceed this number.
+     */
+numberOfLines:_propTypes2['default'].number,
+/**
+     * Invoked on mount and layout changes with
+     *
+     *   `{nativeEvent: {layout: {x, y, width, height}}}`
+     */
+onLayout:_propTypes2['default'].func,
+/**
+     * This function is called on press.
+     */
+onPress:_propTypes2['default'].func,
+/**
+     * When true, no visual change is made when text is pressed down. By
+     * default, a gray oval highlights the text on press down.
+     * @platform ios
+     */
+suppressHighlighting:_propTypes2['default'].bool,
+style:stylePropType,
+/**
+     * Used to locate this view in end-to-end tests.
+     */
+testID:_propTypes2['default'].string,
+/**
+     * Specifies should fonts scale to respect Text Size accessibility setting on iOS.
+     * @platform ios
+     */
+allowFontScaling:_propTypes2['default'].bool},
+
+mixins:[_NativeMethodsMixin2['default']],
+
+render:function(){function render(){
+return null;
+}return render;}()});
+
+
+module.exports=Text;
